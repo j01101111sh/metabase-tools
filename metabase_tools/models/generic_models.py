c37660b359927903daefde87c8ad7e808dbfb9bf
Josh Odell <j01101111sh@gmail.com>
2022-08-03 10:56:43 -0500

    Added generic result class for rest_adapter

--- metabase_tools/models/generic_models.py
+++ metabase_tools/models/generic_models.py
@@ -1,2 +1,14 @@
-class Result:
-    pass
+from pydantic import BaseModel
+from pydantic.fields import Field
+
+
+class Result(BaseModel):
+    """Result returned from RestAdapter
+    Args:
+        status_code (int): Standard HTTP status code
+        message (str, optional): Human readable result. Defaults to ''.
+        data (list[dict], optional): Data payload from response. Defaults to None.
+    """
+    status_code: int
+    message: str = ''
+    data: list[dict] | dict | None = Field(default_factory=list)
