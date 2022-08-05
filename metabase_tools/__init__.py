2769681933bfa4232b0f13ac0e0b372dddc45bc8
Josh Odell <j01101111sh@gmail.com>
2022-08-05 15:24:23 -0500

    Added new exceptions for greater clarity

--- metabase_tools/__init__.py
+++ metabase_tools/__init__.py
@@ -2,6 +2,8 @@ from . import _version
 
 __version__ = _version.get_versions()['version']
 
-from .exceptions import MetabaseApiException
+from .exceptions import (AuthenticationFailure, EmptyDataReceived,
+                         InvalidDataReceived, InvalidParameters,
+                         MetabaseApiException, RequestFailure)
 from .metabase import MetabaseApi
 from .models.card import Card
--- metabase_tools/exceptions.py
+++ metabase_tools/exceptions.py
@@ -1,2 +1,22 @@
 class MetabaseApiException(Exception):
     pass
+
+
+class AuthenticationFailure(MetabaseApiException):
+    pass
+
+
+class EmptyDataReceived(MetabaseApiException):
+    pass
+
+
+class InvalidDataReceived(MetabaseApiException):
+    pass
+
+
+class InvalidParameters(MetabaseApiException):
+    pass
+
+
+class RequestFailure(MetabaseApiException):
+    pass
