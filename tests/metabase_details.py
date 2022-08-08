2513988f2235838948c84db5fa59796b56126231
Josh Odell <j01101111sh@gmail.com>
2022-08-08 16:20:18 -0500

    Moved metabase details to separate file

---
+++ tests/metabase_details.py
@@ -0,0 +1,7 @@
+HOST = 'http://localhost:3000'
+FIRST_NAME = "test"
+LAST_NAME = "test"
+EMAIL = "test@test.com"
+PASSWORD = "Whg6R96c*^fc{um>"
+SITE_NAME = "testing-site"
+CREDENTIALS = {'username': EMAIL, 'password': PASSWORD}
