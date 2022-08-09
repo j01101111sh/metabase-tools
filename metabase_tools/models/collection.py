4a6a10f6688459b9f49c168de89a1f7a06a67673
Josh Odell <j01101111sh@gmail.com>
2022-08-09 15:01:38 -0500

    Basic support for collections

--- metabase_tools/models/collection.py
+++ metabase_tools/models/collection.py
@@ -1,15 +1,37 @@
 from typing import Optional
 
+from typing_extensions import Self
+
+from ..metabase import MetabaseApi
 from .generic import MetabaseGeneric
 
 
 class Collection(MetabaseGeneric):
     description: Optional[str]
-    archived: bool
-    slug: str
-    color: str
+    archived: Optional[bool]
+    slug: Optional[str]
+    color: Optional[str]
     name: str
     personal_owner_id: Optional[int]
-    id: int
-    location: str
+    id: str | int
+    location: Optional[str]
     namespace: Optional[int]
+    effective_location: Optional[str]
+    effective_ancestors: Optional[list[str]]
+    can_write: Optional[bool]
+
+    @classmethod
+    def get(cls, adapter: MetabaseApi, targets: Optional[int | list[int]] = None) -> Self | list[Self]:
+        return super(Collection, cls).get(adapter=adapter, endpoint='/collection', targets=targets)
+
+    @classmethod
+    def post(cls, adapter: MetabaseApi, payloads: dict | list[dict]) -> Self | list[Self]:
+        return super(Collection, cls).post(adapter=adapter, endpoint='/collection', payloads=payloads)
+
+    @classmethod
+    def put(cls, adapter: MetabaseApi, payloads: dict | list[dict]) -> Self | list[Self]:
+        return super(Collection, cls).put(adapter=adapter, endpoint='/collection', payloads=payloads)
+
+    @classmethod
+    def archive(cls, adapter: MetabaseApi, targets: int | list[int], unarchive=False) -> Self | list[Self]:
+        return super(Collection, cls).archive(adapter=adapter, endpoint='/collection', targets=targets, unarchive=unarchive)
---
+++ tests/models/test_collection.py
@@ -0,0 +1,26 @@
+import pytest
+from metabase_tools import Collection, MetabaseApi
+
+
+@pytest.fixture(scope='module')
+def api(host, credentials):
+    return MetabaseApi(metabase_url=host, credentials=credentials, cache_token=True, token_path='./metabase.token')
+
+
+@pytest.fixture(scope='module')
+def credentials():
+    from tests.metabase_details import CREDENTIALS
+    return CREDENTIALS
+
+
+@pytest.fixture(scope='module')
+def host():
+    from tests.metabase_details import HOST
+    return HOST
+
+
+def test_collection_list_all(api):
+    collections = Collection.get(adapter=api)
+    assert isinstance(collections, list)
+    assert all(isinstance(collection, Collection)
+               for collection in collections)
