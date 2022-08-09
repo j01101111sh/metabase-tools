24fa0adaa3003feb2bf7a9981722c973fc868a93
Josh Odell <j01101111sh@gmail.com>
2022-08-09 07:59:03 -0500

    Commented get method

--- metabase_tools/models/generic.py
+++ metabase_tools/models/generic.py
@@ -10,9 +10,33 @@ from ..metabase import MetabaseApi
 class MetabaseGeneric(BaseModel):
     @classmethod
     def get(cls, adapter: MetabaseApi, endpoint: str, targets: Optional[int | list[int]]) -> Self | list[Self]:
+        """Generic method for returning an object or list of objects
+
+        Parameters
+        ----------
+        adapter : MetabaseApi
+            Connection to Metabase API
+        endpoint : str
+            Endpoint to use for the request
+        targets : Optional[int  |  list[int]]
+            If None, return list of the selected objects. Otherwise, return the object(s) of the selected type with the ID(s) provided.
+
+        Returns
+        -------
+        Self | list[Self]
+
+        Raises
+        ------
+        InvalidParameters
+            Targets are not None, int, or list[int]
+        EmptyDataReceived
+            No data is received from the API
+        """
         if isinstance(targets, list) and all(isinstance(t, int) for t in targets):
+            # If a list of targets is provided, return a list of objects
             pass
         elif isinstance(targets, int):
+            # If a single target is provided, return that object
             pass
         elif targets is None:
             # If no targets are provided, all objects of that type should be returned
@@ -21,5 +45,7 @@ class MetabaseGeneric(BaseModel):
                 # Unpack data into instances of the class and return
                 return [cls(**record) for record in response.data]
         else:
+            # If something other than None, int or list[int], raise error
             raise InvalidParameters('Invalid target(s)')
+        # If response.data was empty, raise error
         raise EmptyDataReceived('No data returned')
