c90abb79b90bfa8957b8e605d541539cc2828f1f
Josh Odell <j01101111sh@gmail.com>
2022-08-09 11:17:44 -0500

    Updated rest tests to use fixtures

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -2,16 +2,26 @@ import pytest
 from metabase_tools import RestAdapter
 from metabase_tools.exceptions import AuthenticationFailure
 
-from tests.metabase_details import CREDENTIALS, HOST
+
+@pytest.fixture
+def credentials():
+    from tests.metabase_details import CREDENTIALS
+    return CREDENTIALS
 
 
-def test_auth_success():
-    rest_adapter = RestAdapter(metabase_url=HOST, credentials=CREDENTIALS)
+@pytest.fixture
+def host():
+    from tests.metabase_details import HOST
+    return HOST
+
+
+def test_auth_success(host, credentials):
+    rest_adapter = RestAdapter(metabase_url=host, credentials=credentials)
     assert rest_adapter.get_token() is not None
 
 
-def test_auth_fail():
-    bad_credentials = CREDENTIALS
-    CREDENTIALS['password'] = 'wrong'
+def test_auth_fail(host, credentials):
+    bad_credentials = credentials
+    bad_credentials['password'] = 'wrong'
     with pytest.raises(AuthenticationFailure):
-        rest_adapter = RestAdapter(metabase_url=HOST, credentials=CREDENTIALS)
+        rest_adapter = RestAdapter(metabase_url=host, credentials=credentials)
