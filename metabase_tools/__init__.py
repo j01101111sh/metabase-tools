b398c803e864960abdfe58deabaddf48e4c90916
Josh Odell <j01101111sh@gmail.com>
2022-08-09 20:05:48 -0500

    Removed versioneer

--- metabase_tools/__init__.py
+++ metabase_tools/__init__.py
@@ -1,6 +1,4 @@
-from . import _version
-
-__version__ = _version.get_versions()['version']
+__version__ = '0.2.0'
 
 from .exceptions import (AuthenticationFailure, EmptyDataReceived,
                          InvalidDataReceived, InvalidParameters,
