5b62881a094d7451f90a2e06e38aba37919bc7a6
Josh Odell <j01101111sh@gmail.com>
2022-08-12 16:57:02 -0500

    Removing unfinished functions

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -43,12 +43,3 @@ def test_download_native_queries(tools: MetabaseTools):
     assert size > 0  # File size greater than 0
     assert create_time - now < 2  # file was created in the last 2 seconds
     file.unlink()  # remove created file
-
-
-def test_upload_native_queries_dry_run(tools: MetabaseTools):
-    pass
-    # mapping_path = Path("./dev/files/mapping.json")
-    # results = tools.upload_native_queries(mapping_path=mapping_path, dry_run=True)
-    # assert isinstance(results, list)
-    # assert all(isinstance(result, dict) for result in results)
-    # assert all(result["is_success"] for result in results)
