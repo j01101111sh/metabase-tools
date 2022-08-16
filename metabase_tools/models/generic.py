67cb3e20ac91b5273c982fb915ff6f9c6a19da5c
Josh Odell <j01101111sh@gmail.com>
2022-08-16 12:17:50 -0500

    Refactored generic object for lists

--- metabase_tools/models/generic.py
+++ metabase_tools/models/generic.py
@@ -9,6 +9,44 @@ from metabase_tools.metabase import MetabaseApi
 
 class MetabaseGeneric(BaseModel):
     @classmethod
+    def _request_list(
+        cls,
+        http_method: str,
+        adapter: MetabaseApi,
+        endpoint: str,
+        targets: Optional[list[int]] = None,
+        jsons: Optional[list[dict]] = None,
+    ) -> list[Self]:
+        results = []
+        source = targets or jsons
+        if not source:
+            raise InvalidParameters
+
+        for item in source:
+            if isinstance(item, int):
+                response = adapter.do(
+                    http_method=http_method, endpoint=f"{endpoint}/{item}"
+                )
+            elif isinstance(item, dict):
+                if http_method == "PUT":
+                    response = adapter.do(
+                        http_method=http_method,
+                        endpoint=f"{endpoint}/{item['id']}",
+                        json=item,
+                    )
+                else:
+                    response = adapter.do(
+                        http_method=http_method, endpoint=f"{endpoint}", json=item
+                    )
+            else:
+                raise InvalidParameters
+            if response.data and isinstance(response.data, dict):
+                results.append(cls(**response.data))
+        if len(results) > 0:
+            return results
+        raise EmptyDataReceived("No data returned")
+
+    @classmethod
     def get(
         cls, adapter: MetabaseApi, endpoint: str, targets: Optional[list[int]]
     ) -> list[Self]:
@@ -35,13 +73,12 @@ class MetabaseGeneric(BaseModel):
             No data is received from the API
         """
         if isinstance(targets, list) and all(isinstance(t, int) for t in targets):
-            results = []
-            for target in targets:
-                response = adapter.get(endpoint=f"{endpoint}/{target}")
-                if response.data and isinstance(response.data, dict):
-                    results.append(cls(**response.data))
-            if len(results) > 0:
-                return results
+            return cls._request_list(
+                http_method="GET",
+                adapter=adapter,
+                endpoint=endpoint,
+                targets=targets,
+            )
         elif targets is None:
             # If no targets are provided, all objects of that type should be returned
             response = adapter.get(endpoint=endpoint)
@@ -59,44 +96,33 @@ class MetabaseGeneric(BaseModel):
         cls, adapter: MetabaseApi, endpoint: str, payloads: list[dict]
     ) -> list[Self]:
         # TODO validate params by creating a method in the child class
-        # TODO docstring
-        # TODO refactor for DRY
         if isinstance(payloads, list) and all(isinstance(t, dict) for t in payloads):
             # If a list of targets is provided, return a list of objects
-            results = []
-            for payload in payloads:
-                response = adapter.post(endpoint=f"{endpoint}", json=payload)
-                if response.data and isinstance(response.data, dict):
-                    results.append(cls(**response.data))
-            if len(results) > 0:
-                return results
+            return cls._request_list(
+                http_method="POST",
+                adapter=adapter,
+                endpoint=endpoint,
+                jsons=payloads,
+            )
         else:
             # If something other than dict or list[dict], raise error
             raise InvalidParameters("Invalid target(s)")
-        # If response.data was empty or not a valid type, raise error
-        raise EmptyDataReceived("No data returned")
 
     @classmethod
     def put(
         cls, adapter: MetabaseApi, endpoint: str, payloads: list[dict]
     ) -> list[Self]:
-        # TODO docstring
         if isinstance(payloads, list) and all(isinstance(t, dict) for t in payloads):
             # If a list of targets is provided, return a list of objects
-            results = []
-            for payload in payloads:
-                response = adapter.put(
-                    endpoint=f"{endpoint}/{payload['id']}", json=payload
-                )
-                if response.data and isinstance(response.data, dict):
-                    results.append(cls(**response.data))
-            if len(results) > 0:
-                return results
+            return cls._request_list(
+                http_method="PUT",
+                adapter=adapter,
+                endpoint=endpoint,
+                jsons=payloads,
+            )
         else:
             # If something other than dict or list[dict], raise error
             raise InvalidParameters("Invalid target(s)")
-        # If response.data was empty or not a valid type, raise error
-        raise EmptyDataReceived("No data returned")
 
     @classmethod
     def archive(
@@ -106,18 +132,12 @@ class MetabaseGeneric(BaseModel):
         targets: list[int],
         unarchive: bool,
     ) -> list[Self]:
-        archive_template = {"id": None, "archived": not unarchive}
         if isinstance(targets, list) and all(isinstance(t, int) for t in targets):
-            results = []
-            for target in targets:
-                archive_template["id"] = target
-                response = adapter.put(
-                    endpoint=f"{endpoint}/{target}", json=archive_template
-                )
-                if response.data and isinstance(response.data, dict):
-                    results.append(cls(**response.data))
-            if len(results) > 0:
-                return results
+            return cls._request_list(
+                http_method="PUT",
+                adapter=adapter,
+                endpoint=endpoint,
+                jsons=[{"id": target, "archived": not unarchive} for target in targets],
+            )
         else:
             raise InvalidParameters("Invalid set of targets")
-        raise EmptyDataReceived("No data returned")
