a102ddc00ab49d24014feae73cf75bec3b585ee8
Josh Odell <j01101111sh@gmail.com>
2022-08-19 12:42:49 -0500

    Basic support for user endpoints

--- metabase_tools/models/user.py
+++ metabase_tools/models/user.py
@@ -1,16 +1,66 @@
 from datetime import datetime
 from typing import Optional
 
+from pydantic.fields import Field
+from typing_extensions import Self
+
+from metabase_tools.metabase import MetabaseApi
 from metabase_tools.models.generic import MetabaseGeneric
 
 
 class User(MetabaseGeneric):
-    name: Optional[str]  # TODO generate this
+    name: str = Field(alias="common_name")
     email: str
     first_name: str
-    last_login: datetime
-    is_qbnewb: bool
-    is_superuser: bool
     last_name: str
     date_joined: datetime
-    common_name: str
+    last_login: Optional[datetime]
+    updated_at: Optional[datetime]
+    is_qbnewb: bool
+    is_superuser: bool
+    ldap_auth: Optional[bool]
+    google_auth: Optional[bool]
+    is_active: Optional[bool]
+    locale: Optional[str]
+    group_ids: Optional[list[int]]
+    login_attributes: Optional[list]
+    personal_collection_id: Optional[int]
+
+    @classmethod
+    def get(
+        cls, adapter: MetabaseApi, targets: Optional[list[int]] = None
+    ) -> list[Self]:
+        return super(User, cls).get(adapter=adapter, endpoint="/user", targets=targets)
+
+    @classmethod
+    def post(cls, adapter: MetabaseApi, payloads: list[dict]) -> list[Self]:
+        return super(User, cls).post(
+            adapter=adapter, endpoint="/user", payloads=payloads
+        )
+
+    @classmethod
+    def put(cls, adapter: MetabaseApi, payloads: list[dict]) -> list[Self]:
+        return super(User, cls).put(
+            adapter=adapter, endpoint="/user", payloads=payloads
+        )
+
+    @classmethod
+    def archive(
+        cls, adapter: MetabaseApi, targets: list[int], unarchive=False
+    ) -> list[Self]:
+        return super(User, cls).archive(
+            adapter=adapter, endpoint="/user", targets=targets, unarchive=unarchive
+        )
+
+    @classmethod
+    def search(
+        cls,
+        adapter: MetabaseApi,
+        search_params: list[dict],
+        search_list: Optional[list] = None,
+    ) -> list[Self]:
+        return super(User, cls).search(
+            adapter=adapter,
+            search_params=search_params,
+            search_list=search_list,
+        )
