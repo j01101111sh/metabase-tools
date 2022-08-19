9da60ba3aeb612761c67c1e687ec45f1d1db3eac
Josh Odell <j01101111sh@gmail.com>
2022-08-19 13:28:16 -0500

    Docstrings on user endpoints

--- metabase_tools/models/user.py
+++ metabase_tools/models/user.py
@@ -1,3 +1,6 @@
+"""User class for user objects from Metabase
+"""
+
 from datetime import datetime
 from typing import Optional
 
@@ -10,6 +13,8 @@ from metabase_tools.models.generic import MetabaseGeneric
 
 
 class User(MetabaseGeneric):
+    """Class for user objects from Metabase"""
+
     name: str = Field(alias="common_name")
     email: str
     first_name: str
@@ -31,16 +36,58 @@ class User(MetabaseGeneric):
     def get(
         cls, adapter: MetabaseApi, targets: Optional[list[int]] = None
     ) -> list[Self]:
+        """Fetch a list of users using the provided MetabaseAPI
+
+        Parameters
+        ----------
+        adapter : MetabaseApi
+            Connection to Metabase API
+        targets : list[int], optional
+            List of targets to fetch. Returns all users if not provided.
+
+        Returns
+        -------
+        list[Self]
+            List of users
+        """
         return super(User, cls).get(adapter=adapter, endpoint="/user", targets=targets)
 
     @classmethod
     def post(cls, adapter: MetabaseApi, payloads: list[dict]) -> list[Self]:
+        """Create new user(s)
+
+        Parameters
+        ----------
+        adapter : MetabaseApi
+            Connection to Metabase API
+        payloads : list[dict]
+            List of dicts with details for new user(s)
+
+        Returns
+        -------
+        list[Self]
+            List of created users
+        """
         return super(User, cls).post(
             adapter=adapter, endpoint="/user", payloads=payloads
         )
 
     @classmethod
     def put(cls, adapter: MetabaseApi, payloads: list[dict]) -> list[Self]:
+        """Update existing user(s)
+
+        Parameters
+        ----------
+        adapter : MetabaseApi
+            Connection to Metabase API
+        payloads : list[dict]
+            List of dicts with details for user update(s)
+
+        Returns
+        -------
+        list[Self]
+            List of user(s) updated
+        """
         return super(User, cls).put(
             adapter=adapter, endpoint="/user", payloads=payloads
         )
@@ -52,6 +99,22 @@ class User(MetabaseGeneric):
         search_params: list[dict],
         search_list: Optional[list] = None,
     ) -> list[Self]:
+        """Search for users based on provided criteria
+
+        Parameters
+        ----------
+        adapter : MetabaseApi
+            Connection to Metabase API
+        search_params : list[dict]
+            List of dicts, each containing search criteria. 1 result returned per dict.
+        search_list : Optional[list[Self]], optional
+            Provide to search against an existing list, by default pulls from API
+
+        Returns
+        -------
+        list[Self]
+            List of users from results
+        """
         return super(User, cls).search(
             adapter=adapter,
             search_params=search_params,
@@ -60,6 +123,18 @@ class User(MetabaseGeneric):
 
     @classmethod
     def current(cls, adapter: MetabaseApi) -> list[Self]:
+        """Fetch the current user
+
+        Parameters
+        ----------
+        adapter : MetabaseApi
+            Connection to Metabase API
+
+        Returns
+        -------
+        list[Self]
+            Current user details
+        """
         response = adapter.get(endpoint="/user/current")
         if response.data:
             return [cls(**record) for record in [response.data]]  # type: ignore
@@ -67,6 +142,20 @@ class User(MetabaseGeneric):
 
     @classmethod
     def disable(cls, adapter: MetabaseApi, targets: list[int]) -> list[Self]:
+        """Disables user(s) provided
+
+        Parameters
+        ----------
+        adapter : MetabaseApi
+            Connection to Metabase API
+        targets : list[int]
+            List of users to disable
+
+        Returns
+        -------
+        list[Self]
+            List of users that were disabled
+        """
         return super(User, cls).delete(
             adapter=adapter, endpoint="/user", targets=targets
         )
