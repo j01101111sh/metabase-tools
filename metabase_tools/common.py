a87be35257f518a18e332332146537c8416b71fb
Josh <j01101111sh@gmail.com>
2022-09-07 16:11:34 -0500

    Reorganized logging helpers

--- metabase_tools/common.py
+++ metabase_tools/common.py
@@ -8,23 +8,39 @@ from typing import Any, Callable, TypeVar
 T = TypeVar("T")
 
 
+def log_details(
+    logger: logging.Logger, func: Callable[..., T], *args: Any, **kwargs: Any
+) -> T:
+    """Logs details of a function call
+
+    Args:
+        logger (logging.Logger)
+        func (Callable[..., T])
+
+    Returns:
+        T
+    """
+    logger = logging.getLogger(func.__module__)
+    logger.debug(
+        "%s called\n\targs: %s\n\tkwargs: %s",
+        func.__name__,
+        "\n\t\t".join([str(s) for s in args]),
+        "\n\t\t".join(
+            [f"{key}: {value}" for key, value in kwargs.items()],
+        ),
+    )
+    return_ = func(*args, **kwargs)
+    logger.debug("Returning: %s", return_)
+    return return_
+
+
 def log_call(func: Callable[..., T]) -> Callable[..., T]:
     """Used to log calls to the function provided"""
 
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> T:
         logger = logging.getLogger(func.__module__)
-        logger.debug(
-            "%s called\n\targs: %s\n\tkwargs: %s",
-            func.__name__,
-            "\n\t\t".join([str(s) for s in args]),
-            "\n\t\t".join(
-                [f"{key}: {value}" for key, value in kwargs.items()],
-            ),
-        )
-        return_ = func(*args, **kwargs)
-        logger.debug("Returning: %s", return_)
-        return return_
+        return log_details(logger, func, *args, **kwargs)
 
     return wrapper
 
@@ -36,16 +52,6 @@ def untested(func: Callable[..., T]) -> Callable[..., T]:
     def wrapper(*args: Any, **kwargs: Any) -> T:
         logger = logging.getLogger(func.__module__)
         logger.warning("Calling untested function: %s", func.__name__)
-        logger.debug(
-            "%s called\n\targs: %s\n\tkwargs: %s",
-            func.__name__,
-            "\n\t\t".join([str(s) for s in args]),
-            "\n\t\t".join(
-                [f"{key}: {value}" for key, value in kwargs.items()],
-            ),
-        )
-        return_ = func(*args, **kwargs)
-        logger.debug("Returning: %s", return_)
-        return return_
+        return log_details(logger=logger, func=func, args=args, kwargs=kwargs)
 
     return wrapper
