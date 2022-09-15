cdaa7f0d4ccdb61bcacbab79b11ede2922b9e57d
Josh <j01101111sh@gmail.com>
2022-09-15 10:13:33 -0500

    Removed unused fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,18 +4,5 @@ import pytest
 from metabase_tools import MetabaseApi
 
 
-@pytest.fixture
-def token(host, credentials, result_path, run_id):
-    token_path = f"{result_path}/{run_id}.token"
-    _ = MetabaseApi(
-        metabase_url=host,
-        credentials=credentials,
-        cache_token=True,
-        token_path=token_path,
-    )
-    with open(token_path, "r", encoding="utf-8") as file:
-        return {"token": file.read()}
-
-
 def test_server_version(api: MetabaseApi):
     assert isinstance(api.server_version, packaging.version.Version)
