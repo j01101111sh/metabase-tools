0321043d8e2b27516b6348b76a2d5b804def83ce
Josh <j01101111sh@gmail.com>
2022-09-15 10:24:52 -0500

    Fixed missing fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,20 @@ from pathlib import Path
 import pytest
 
 from metabase_tools import MetabaseApi
+from tests import helpers
 
 _run_id = datetime.now().strftime("%y%m%dT%H%M%S")
 _result_path = Path(f"./temp/test-{_run_id}")
+
+
+@pytest.fixture(scope="session")
+def credentials():
+    return helpers.CREDENTIALS
+
+
+@pytest.fixture(scope="session")
+def host():
+    return helpers.HOST
 
 
 @pytest.fixture(scope="package")
