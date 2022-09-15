92e14b79c1ba10c66657aca13abfde11da9e29e4
Josh <j01101111sh@gmail.com>
2022-09-15 10:22:18 -0500

    Removed more unused fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,16 +12,6 @@ _python_version = python_version().replace(".", "_")
 _result_path = Path(f"./temp/test-{_run_id}/python_{_python_version}")
 
 
-@pytest.fixture(scope="session")
-def run_id() -> str:
-    return _run_id
-
-
-@pytest.fixture(scope="session")
-def result_path() -> Path:
-    return _result_path
-
-
 @pytest.fixture(scope="package")
 def api(host, credentials):
     token_path = f"{_result_path}/{_run_id}.token"
@@ -31,37 +21,3 @@ def api(host, credentials):
         cache_token=True,
         token_path=token_path,
     )
-
-
-@pytest.fixture(scope="package")
-def server_version(api: MetabaseApi):
-    return api.server_version
-
-
-@pytest.fixture(scope="session")
-def credentials():
-    return helpers.CREDENTIALS
-
-
-@pytest.fixture(scope="session")
-def host():
-    return helpers.HOST
-
-
-@pytest.fixture(scope="session")
-def email():
-    return helpers.EMAIL
-
-
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_setup(item):
-    config = item.config
-    logging_plugin = config.pluginmanager.get_plugin("logging-plugin")
-
-    test_result_path = (
-        item.nodeid.split(".py", 1)[0].replace("tests/", "logs/") + f"/{item.name}"
-    )
-
-    full_fname = Path(f"{_result_path}/{test_result_path}.log")
-    logging_plugin.set_log_path(full_fname)
-    yield
