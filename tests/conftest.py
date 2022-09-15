3d3729b402fb576ee1b733d7362fb57c4ac3c28e
Josh <j01101111sh@gmail.com>
2022-09-15 10:22:51 -0500

    Removed python version from result path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,12 @@
 from datetime import datetime
 from pathlib import Path
-from platform import python_version
 
 import pytest
 
 from metabase_tools import MetabaseApi
-from tests import helpers
 
 _run_id = datetime.now().strftime("%y%m%dT%H%M%S")
-_python_version = python_version().replace(".", "_")
-_result_path = Path(f"./temp/test-{_run_id}/python_{_python_version}")
+_result_path = Path(f"./temp/test-{_run_id}")
 
 
 @pytest.fixture(scope="package")
