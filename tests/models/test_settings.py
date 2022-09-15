9cc8afdae228928f1585abca198339b596a4e4c3
Josh <j01101111sh@gmail.com>
2022-09-15 09:34:44 -0500

    Testing removing test files

--- tests/models/test_settings.py
+++
@@ -1,9 +0,0 @@
-from metabase_tools import MetabaseApi
-from metabase_tools.server_settings import ServerSettings, Setting
-
-
-def test_update_admin_email(api: MetabaseApi):
-    result = api.settings.admin_email.update("jim.test@dundermifflin.com")
-    assert result["success"] is True
-    assert isinstance(api.settings, ServerSettings)
-    assert isinstance(api.settings.admin_email, Setting)
