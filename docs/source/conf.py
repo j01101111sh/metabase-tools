13ae2179ec75a27cc9d2ee3e6710e34a998333a3
Josh <j01101111sh@gmail.com>
2022-09-15 13:18:13 -0500

    Updated static path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,4 +35,4 @@ autodoc_typehints = "description"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = ["examples"]
