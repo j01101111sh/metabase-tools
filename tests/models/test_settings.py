cd1c11b619407bca1da29dfff5b207a388c1b741
Josh <j01101111sh@gmail.com>
2022-09-16 11:24:27 -0500

    Added new test for setting updates

--- tests/models/test_settings.py
+++ tests/models/test_settings.py
@@ -1,3 +1,5 @@
+import pytest
+
 from metabase_tools import MetabaseApi
 from metabase_tools.server_settings import ServerSettings, Setting
 
@@ -7,3 +9,8 @@ def test_update_admin_email(api: MetabaseApi):
     assert result["success"] is True
     assert isinstance(api.settings, ServerSettings)
     assert isinstance(api.settings.admin_email, Setting)
+
+
+def test_update_wrong_type(api: MetabaseApi):
+    with pytest.raises(TypeError):
+        _ = api.settings.enable_embedding.update("String")
