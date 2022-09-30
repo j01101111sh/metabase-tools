82375766f99c85cdb16a6893b22686eaaed58379
Josh <j01101111sh@gmail.com>
2022-09-29 22:05:01 -0500

    Added classes to remaining tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from packaging.version import Version
 import pytest
+from packaging.version import Version
 
 from metabase_tools import MetabaseApi
 from metabase_tools.exceptions import AuthenticationFailure
@@ -20,119 +20,113 @@ def token(host, credentials, result_path, run_id):
         return {"token": file.read()}
 
 
-def test_auth_credential_success(host, credentials):
-    api = MetabaseApi(
-        metabase_url=host, credentials=credentials, token_path="./missing.token"
-    )
-    assert api.test_for_auth()
-
-
-def test_auth_credential_fail(host, email):
-    bad_credentials = {"username": email, "password": "badpass"}
-    with pytest.raises(AuthenticationFailure):
-        _ = MetabaseApi(
-            metabase_url=host, credentials=bad_credentials, token_path="./missing.token"
-        )
-
-
-def test_auth_token_success(host, token, email):
-    api = MetabaseApi(
-        metabase_url=host, credentials=token, token_path="./missing.token"
-    )
-    test_response = api.get(endpoint="/user/current")
-    if isinstance(test_response, dict):
-        assert test_response.get("email") == email
-    else:
-        assert False
-
-
-def test_auth_token_fail(host):
-    bad_credentials = {"token": "badtoken"}
-    with pytest.raises(AuthenticationFailure):
+class TestApiGeneral:
+    def test_auth_credential_success(self, host: str, credentials: dict):
         api = MetabaseApi(
-            metabase_url=host, credentials=bad_credentials, token_path="./missing.token"
+            metabase_url=host, credentials=credentials, token_path="./missing.token"
         )
-        _ = api.get(endpoint="/user/current")
+        assert api.test_for_auth()
 
+    def test_auth_credential_fail(self, host: str, email: str):
+        bad_credentials = {"username": email, "password": "badpass"}
+        with pytest.raises(AuthenticationFailure):
+            _ = MetabaseApi(
+                metabase_url=host,
+                credentials=bad_credentials,
+                token_path="./missing.token",
+            )
 
-def test_auth_token_file_success(api, host, result_path, run_id):
-    _ = api
-    token_path = Path(f"{result_path}/{run_id}.token")
-    new_api = MetabaseApi(metabase_url=host, token_path=token_path)
-    assert new_api.test_for_auth()
+    def test_auth_token_success(self, host: str, token: dict, email: str):
+        api = MetabaseApi(
+            metabase_url=host, credentials=token, token_path="./missing.token"
+        )
+        test_response = api.get(endpoint="/user/current")
+        if isinstance(test_response, dict):
+            assert test_response.get("email") == email
+        else:
+            assert False
 
+    def test_auth_token_fail(self, host: str):
+        bad_credentials = {"token": "badtoken"}
+        with pytest.raises(AuthenticationFailure):
+            api = MetabaseApi(
+                metabase_url=host,
+                credentials=bad_credentials,
+                token_path="./missing.token",
+            )
+            _ = api.get(endpoint="/user/current")
 
-def test_auth_token_file_fail(host):
-    with pytest.raises(AuthenticationFailure):
-        _ = MetabaseApi(metabase_url=host, token_path="./missing.token")
+    def test_auth_token_file_success(
+        self, api: MetabaseApi, host: str, result_path: str, run_id: str
+    ):
+        _ = api
+        token_path = Path(f"{result_path}/{run_id}.token")
+        new_api = MetabaseApi(metabase_url=host, token_path=token_path)
+        assert new_api.test_for_auth()
 
+    def test_auth_token_file_fail(self, host: str):
+        with pytest.raises(AuthenticationFailure):
+            _ = MetabaseApi(metabase_url=host, token_path="./missing.token")
 
-def test_auth_without_protocol(host, credentials):
-    api = MetabaseApi(metabase_url=host[7:], credentials=credentials)
-    assert api.test_for_auth()
+    def test_auth_without_protocol(self, host: str, credentials: dict):
+        api = MetabaseApi(metabase_url=host[7:], credentials=credentials)
+        assert api.test_for_auth()
 
+    def test_cache_token(
+        self, host: str, credentials: dict, result_path: str, run_id: str, token: dict
+    ):
+        token_path = f"{result_path}/{run_id}.token"
+        api = MetabaseApi(
+            metabase_url=host,
+            credentials=credentials,
+            cache_token=True,
+            token_path=token_path,
+        )
+        with open(token_path, "r", encoding="utf-8") as file:
+            cached_token = {"token": file.read()}
+        assert cached_token == token
+        assert api.test_for_auth()
 
-def test_cache_token(host, credentials, result_path, run_id, token):
-    token_path = f"{result_path}/{run_id}.token"
-    api = MetabaseApi(
-        metabase_url=host,
-        credentials=credentials,
-        cache_token=True,
-        token_path=token_path,
-    )
-    with open(token_path, "r", encoding="utf-8") as file:
-        cached_token = {"token": file.read()}
-    assert cached_token == token
-    assert api.test_for_auth()
+    def test_fail_on_no_credentials(self, host: str):
+        with pytest.raises(AuthenticationFailure):
+            _ = MetabaseApi(metabase_url=host)
 
+    def test_url_ends_in_slash(self, host: str, credentials: dict):
+        api = MetabaseApi(metabase_url=f"{host}/", credentials=credentials)
+        assert api.test_for_auth()
 
-def test_fail_on_no_credentials(host):
-    with pytest.raises(AuthenticationFailure):
-        _ = MetabaseApi(metabase_url=host)
+    def test_url_ends_in_api(self, host: str, credentials: dict):
+        api = MetabaseApi(metabase_url=f"{host}/api", credentials=credentials)
+        assert api.test_for_auth()
 
+    def test_url_ends_in_api_and_slash(self, host: str, credentials: dict):
+        api = MetabaseApi(metabase_url=f"{host}/api/", credentials=credentials)
+        assert api.test_for_auth()
 
-def test_url_ends_in_slash(host, credentials):
-    api = MetabaseApi(metabase_url=f"{host}/", credentials=credentials)
-    assert api.test_for_auth()
+    def test_bad_cached_token_erased(self, host: str, credentials: dict, caplog):
+        bad_token = "badtoken"
+        bad_token_path = Path("bad_token.token")
+        with open(bad_token_path, "w", encoding="utf-8") as file:
+            file.write(bad_token)
+        api = MetabaseApi(
+            metabase_url=host, credentials=credentials, token_path=bad_token_path
+        )
+        assert api.test_for_auth()
+        assert not bad_token_path.exists()
+        assert "Deleting token file" in caplog.text
 
+    def test_bad_passed_token(self, host: str, caplog):
+        bad_token = {"token": "badtoken"}
+        with pytest.raises(AuthenticationFailure):
+            _ = MetabaseApi(metabase_url=host, credentials=bad_token)
+        assert "Failed to authenticate with token passed" in caplog.text
 
-def test_url_ends_in_api(host, credentials):
-    api = MetabaseApi(metabase_url=f"{host}/api", credentials=credentials)
-    assert api.test_for_auth()
+    def test_bad_passed_token_with_fallback(self, host: str, credentials: dict, caplog):
+        credentials["token"] = "badtoken"
+        api = MetabaseApi(metabase_url=host, credentials=credentials)
+        assert api.test_for_auth()
+        assert "Failed to authenticate with token passed" in caplog.text
+        assert "Authenticated with login" in caplog.text
 
-
-def test_url_ends_in_api_and_slash(host, credentials):
-    api = MetabaseApi(metabase_url=f"{host}/api/", credentials=credentials)
-    assert api.test_for_auth()
-
-
-def test_bad_cached_token_erased(host, credentials, caplog):
-    bad_token = "badtoken"
-    bad_token_path = Path("bad_token.token")
-    with open(bad_token_path, "w", encoding="utf-8") as file:
-        file.write(bad_token)
-    api = MetabaseApi(
-        metabase_url=host, credentials=credentials, token_path=bad_token_path
-    )
-    assert api.test_for_auth()
-    assert not bad_token_path.exists()
-    assert "Deleting token file" in caplog.text
-
-
-def test_bad_passed_token(host, caplog):
-    bad_token = {"token": "badtoken"}
-    with pytest.raises(AuthenticationFailure):
-        _ = MetabaseApi(metabase_url=host, credentials=bad_token)
-    assert "Failed to authenticate with token passed" in caplog.text
-
-
-def test_bad_passed_token_with_fallback(host, credentials, caplog):
-    credentials["token"] = "badtoken"
-    api = MetabaseApi(metabase_url=host, credentials=credentials)
-    assert api.test_for_auth()
-    assert "Failed to authenticate with token passed" in caplog.text
-    assert "Authenticated with login" in caplog.text
-
-
-def test_server_version(api: MetabaseApi):
-    assert isinstance(api.server_version, Version)
+    def test_server_version(self, api: MetabaseApi):
+        assert isinstance(api.server_version, Version)
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,15 +4,17 @@ from metabase_tools import MetabaseApi
 from metabase_tools.models import ServerSettings, Setting
 
 
-def test_update_site_name(api: MetabaseApi):
-    new_site_name = "testing-site-test"
-    result = api.settings.site_name.update(new_site_name)
-    assert result["success"] is True
-    assert isinstance(api.settings, ServerSettings)
-    assert isinstance(api.settings.site_name, Setting)
-    assert api.settings.site_name.value == new_site_name
+class TestSettingsPass:
+    def test_update_site_name(self, api: MetabaseApi):
+        new_site_name = "testing-site-test"
+        result = api.settings.site_name.update(new_site_name)
+        assert result["success"] is True
+        assert isinstance(api.settings, ServerSettings)
+        assert isinstance(api.settings.site_name, Setting)
+        assert api.settings.site_name.value == new_site_name
 
 
-def test_update_wrong_type(api: MetabaseApi):
-    with pytest.raises(TypeError):
-        _ = api.settings.site_name.update(4)
+class TestSettingsFail:
+    def test_update_wrong_type(self, api: MetabaseApi):
+        with pytest.raises(TypeError):
+            _ = api.settings.site_name.update(4)
