7bfd8729baef4265e21aaa8e67f558930c87cc33
Josh Odell <j01101111sh@gmail.com>
2022-11-10 21:54:26 -0600

    Added search method on activity

--- metabase_tools/endpoints/activity_endpoint.py
+++ metabase_tools/endpoints/activity_endpoint.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 from logging import getLogger
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING, Any, ClassVar, Optional
 
 from metabase_tools.exceptions import MetabaseApiException
 from metabase_tools.models.activity_model import ActivityItem
@@ -40,3 +40,29 @@ class Activity:
                 activity.set_adapter(self._adapter)
             return activities
         raise MetabaseApiException
+
+    def search(
+        self,
+        search_params: list[dict[str, Any]],
+        search_list: Optional[list[ActivityItem]] = None,
+    ) -> list[ActivityItem]:
+        """Method to search a list of objects meeting a list of parameters
+
+        Args:
+            search_params (list[dict]): Each dict contains search criteria and returns\
+                 1 result
+            search_list (list[T], optional): Provide to search against an existing \
+                list, by default pulls from API
+
+        Returns:
+            list[T]: List of objects of the relevant type
+        """
+        objs = search_list or self.get()
+        results = []
+        for param in search_params:
+            for obj in objs:
+                obj_dict = obj.dict()
+                for key, value in param.items():
+                    if key in obj_dict and value == obj_dict[key]:
+                        results.append(obj)
+        return results
