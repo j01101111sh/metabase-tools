80d52868b6ba71fab85033e7a1546aa82ba956b8
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2023-10-21 21:25:22 +0000

    bumped version to 0.16

--- metabase_tools/__init__.py
+++ metabase_tools/__init__.py
@@ -1,7 +1,7 @@
 """Unofficial API wrapper for Metabase
 """
 
-__version__ = "0.15.1"
+__version__ = "0.16.0"
 
 from metabase_tools.exceptions import MetabaseApiException
 from metabase_tools.metabase import MetabaseApi
