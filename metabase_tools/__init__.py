22ab30436b72a53f2a8a1562e83431a55f763213
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2023-10-22 19:05:23 +0000

    Added run time type checking

--- metabase_tools/__init__.py
+++ metabase_tools/__init__.py
@@ -3,7 +3,11 @@
 
 __version__ = "0.16.0"
 
+from beartype.claw import beartype_this_package
+
 from metabase_tools.exceptions import MetabaseApiException
 from metabase_tools.metabase import MetabaseApi
 
 __all__ = ("MetabaseApiException", "MetabaseApi")
+
+beartype_this_package()
